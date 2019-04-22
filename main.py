32d2da17eb55d332007779e1bc41e38772d62fc5
WeiqiuChen <550481591@qq.com>
2019-04-22 15:40:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+# -*- coding: utf-8 -*-
 #version:ython 2.7
 #create time:14:17 2019-04-22
 #update time:
 #author:cwq
 
-#code
+
 import tornado.ioloop
 import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        import time
+        self.write("Hello world!\nI am chenweiqiu!")
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Index!!!")
+
+application = tornado.web.Application([
+    (r"/main", MainHandler),
+    (r"/index", IndexHandler),
+])
+
+if __name__ == "__main__":
+    print "start web sever!!!"
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
